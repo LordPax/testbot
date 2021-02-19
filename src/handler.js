e329bae00666072453638501a5fc7fc6fb76e40d
LordPax <capitaine_ted@hotmail.fr>
2021-02-19 18:23:36 +0100

    feat : message template

--- src/handler.js
+++ src/handler.js
@@ -10,6 +10,31 @@ const handleMessage = (sender_psid, received_msg) => {
             text : `You sent the message: "${received_msg.text}". Now send me an image!`
         }
     }
+    else if (received_msg.attachments) {
+        let attachment_url = received_msg.attachments[0].payload.url
+        response = {
+            type : 'template',
+            payload : {
+                template_type : 'generic',
+                elements : [{
+                    title : "c'est la bonne image ?",
+                    subtitle : 'click pour répondre',
+                    image_url : attachment_url,
+                    buttons : [{
+                        type : 'postback',
+                        title : 'Oui',
+                        payload : 'oui'
+                    }, 
+                    {
+                        type : 'postback',
+                        title : 'Non',
+                        payload : 'non'
+                    }]
+                }]
+            }
+        }
+    }
+
     callSendAPI(sender_psid, response)
 }
 
