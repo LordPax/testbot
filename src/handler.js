04ea48ff688e7f13f71472937a3ed8d5fe5fffdc
LordPax <capitaine_ted@hotmail.fr>
2021-02-19 12:36:08 +0100

    fix : json

--- src/handler.js
+++ src/handler.js
@@ -7,7 +7,7 @@ const handleMessage = (sender_psid, received_msg) => {
     let response
     if (received_msg.text) {
         response = {
-            "text" : `You sent the message: "${received_msg.text}". Now send me an image!`
+            text : `You sent the message: "${received_msg.text}". Now send me an image!`
         }
     }
     callSendAPI(sender_psid, response)
@@ -19,23 +19,17 @@ const handlePostback = (sender_psid, received_postback) => {
 
 const callSendAPI = (sender_psid, response) => {
     const request_body = {
-        "recipient" : {
-            "id" : sender_psid
+        recipient : {
+            id : sender_psid
         },
-        "message" : response
+        message : response
     }
 
     request({
-        "uri" : 'https://graph.facebook.com/v2.6/me/messages',
-        "qs" : { "access_token" : ACCESS_TOKEN },
-        "method" : 'POST',
-        "json" : request_body
-    }, (err, res, body) => {
-        if (!err) {
-            console.log('message sent!')
-        } else {
-            console.error("Unable to send message:" + err);
-        }
+        uri : 'https://graph.facebook.com/v2.6/me/messages',
+        qs : { access_token : ACCESS_TOKEN },
+        method : 'POST',
+        json : request_body
     })
 }
 
